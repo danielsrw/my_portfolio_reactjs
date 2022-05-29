4c6c006afeaee4c044845d0f16a5bf14c195612c
danielsrw <danielsrwanda@gmail.com>
2022-05-29 16:58:18 +0200

    New release

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -68,7 +68,7 @@ const Header = () => (
             whileInView={scaleVariants.whileInView}
             className="app__header-circles"
         >
-            {[images.python, images.laravel, images.sass, images.sanity].map((circle, index) => (
+            {[images.python, images.laravel, images.sass, images.node].map((circle, index) => (
                 <div className="circle-cmp app__flex" key={`circle-${index}`}>
                     <img src={circle} alt="profile_bg" />
                 </div>
