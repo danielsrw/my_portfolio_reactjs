fb26ca8b14aa80cf10795f4d8922fdc37bb54fea
danielsrw <danielsrwanda@gmail.com>
2022-06-19 19:33:05 +0200

    initial release

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -60,6 +60,49 @@ const Work = () => {
                 {/* {filterWork.map((work, index) => ( */}
                     <div className="app__work-item app__flex">
                         <div className="app__work-img app__flex" >
+                            <img src={images.gym} alt='Name' />
+
+                            <motion.div
+                                whileHover={{ opacity: [0, 1] }}
+                                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                                className="app__work-hover app__flex"
+                            >
+                                <a href='https://gym-react-app-250.vercel.app/' target="_blank" rel="noreferrer">
+                                    <motion.div
+                                        whileInView={{ scale: [0, 1] }}
+                                        whileHover={{ scale: [1, 0.90] }}
+                                        transition={{ duration: 0.25 }}
+                                        className="app__flex"
+                                    >
+                                        <AiFillEye />
+                                    </motion.div>
+                                </a>
+                                <a href='https://github.com/danielsrw/gym-react-app' target="_blank" rel="noreferrer">
+                                    <motion.div
+                                        whileInView={{ scale: [0, 1] }}
+                                        whileHover={{ scale: [1, 0.90] }}
+                                        transition={{ duration: 0.25 }}
+                                        className="app__flex"
+                                    >
+                                    <AiFillGithub />
+                                </motion.div>
+                                </a>
+                            </motion.div>
+                        </div>
+
+                        <div className="app__work-content app__flex">
+                            <h4 className="bold-text">GYM React JS App</h4>
+                            <p className="p-text" style={{ marginTop: 10 }}>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex, quaerat?
+                            </p>
+
+                            <div className="app__work-tag app__flex">
+                                <p className="p-text">Web App</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="app__work-item app__flex">
+                        <div className="app__work-img app__flex" >
                             <img src={images.netflix} alt='Name' />
 
                             <motion.div
