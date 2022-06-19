5494a955f41f4781908a7422bb711e196a51557a
danielsrw <danielsrwanda@gmail.com>
2022-06-19 19:52:58 +0200

    initial release

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -3,17 +3,17 @@ import { BsTwitter, BsInstagram, BsWhatsapp } from 'react-icons/bs';
 
 const SocialMedia = () => (
     <div className="app__social">
-        <a href="https://twitter.com/anonymousxviiii">
+        <a href="https://twitter.com/dvniels_">
             <div>
                 <BsTwitter />
             </div>
         </a>
-        <a href="https://instagram.com/daniels_xvii">
+        <a href="https://www.instagram.com/dvniels_xx">
             <div>
                 <BsInstagram />
             </div>
         </a>
-        <a href="">
+        <a href="https://wa.me/250781862349">
             <div>
                 <BsWhatsapp />
             </div>
